62afcecd05881a1c55a3ac504921b126dc7fef95
Phi Hung <phihungqaz123@gmail.com>
2024-11-18 21:09:50 +0700

    Update MainActivity.java

--- app/src/main/java/com/app/gasstore/MainActivity.java
+++ app/src/main/java/com/app/gasstore/MainActivity.java
@@ -19,19 +19,7 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-<<<<<<< HEAD
 
-
-
-
-            
-=======
-<<<<<<< HEAD
-
-
-//nam comment
-
->>>>>>> nam
         });
     }
 }
